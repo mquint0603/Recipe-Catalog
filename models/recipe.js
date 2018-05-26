5cfe0c9953ba1a2586d4ab81d34cc8d30b45248a
adrianarmiller <adriana.rmiller@gmail.com>
2018-05-26 10:34:23 -0500

    Database structure, sequelize setup

---
+++ models/recipe.js
@@ -0,0 +1,42 @@
+module.exports = function(sequelize, DataTypes) {
+    var Recipe = sequelize.define("Recipe", {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      ingredients: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        len: [1]
+      },
+      directions: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        len: [1]
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        len: [1]
+      },
+      category: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        len: [1]
+      }
+    });
+  
+    Recipe.associate = function(models) {
+      Recipe.belongsTo(models.Chef, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+  
+    return Recipe;
+  };
+  
