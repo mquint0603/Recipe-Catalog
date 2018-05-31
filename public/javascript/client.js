1cf1fd0d2fb4635dc035d41674df9155ed880614
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-05-30 20:18:21 -0500

    added js to ajax call

---
+++ public/javascript/client.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+
+    $("#postSubmitButton").on("click", function(event) {
+        
+        let keywordString = $("#keyword").val().trim();
+        let keywords = keywordString.split(",")
+
+        var recipePost = {
+            title: $("#title").val().trim(),
+            ingredients: $("#ingredients").val().trim(),
+            directions: $("#directions").val().trim(),
+            description: $("#description").val().trim(),
+            category: $("#category").val().trim(),
+            keywords: keywords
+        }
+
+        $.ajax("/api/recipes", {
+        type: "POST",
+        data: recipePost
+        }).then(
+        function() {
+            console.log("new recipe posted");
+
+            location.reload();
+        }
+        );
+    })
+
+    $("#searchSubmitButton").on("click", function(event) {
+        var searchQuery = {
+            category: $("#search-category").val(),
+            search: $("#search-box").val().trim()
+        }
+
+        $.ajax("/api/recipes/keyword/" + searchQuery.search, {
+            type: "GET",
+            data: searchQuery
+            }).then(
+            function() {
+                console.log("new search query");
+               
+                location.reload();
+            }
+        );
+    })
+})
