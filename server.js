5c0b60907b87e98fc79ff39efa32f8a49af86ccb
lwerner27 <lwerner27@gmail.com>
2018-06-04 09:40:26 -0500

    changed some code on the server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ require("./controllers/htmlRouter.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: false }).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
